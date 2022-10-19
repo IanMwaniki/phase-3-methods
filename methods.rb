85dd6f35a84d32ce6a616424b7148df942ed826c
Ian Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-10-19 05:29:45 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet (name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet
+greet "Jimmy"
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default "Naureen"
+
+def add_and_log(num1, num2)
+    puts num1 + num2
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+ add(2, 5)
+
+ def halve(name)
+  
+    halve(num1, num2)
+    return num1/num2
+
+    if name.class != ""
+    return nil
+ end
+
+ halve(6/2)
